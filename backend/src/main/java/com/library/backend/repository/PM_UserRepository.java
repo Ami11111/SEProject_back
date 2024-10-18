290eab3751c16cfbf2d3baf9dd6feffed1883a6c
LiangZhiyuan <21307130113@m.fudan.edu.cn>
2024-10-18 21:49:08 +0800

    修改用户信息和修改用户名

--- backend/src/main/java/com/library/backend/repository/PM_UserRepository.java
+++ backend/src/main/java/com/library/backend/repository/PM_UserRepository.java
@@ -2,18 +2,23 @@ package com.library.backend.repository;
 
 import com.library.backend.entity.PM_User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
-public interface PM_UserRepository extends JpaRepository<PM_User, String> {
+public interface PM_UserRepository extends JpaRepository<PM_User, Long> {
 
     int countByUsernameAndPassword(String name, String password);
 
     PM_User findByUsername(String name);
 
     @Transactional
+    @Modifying
     void deleteById(int id);
+    @Transactional
+    @Modifying
+    void updateUsernameById(String usrname, int id);
 
     List<PM_User> findAllByUsernameContaining(String name);
 }
