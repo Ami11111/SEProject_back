7008ad73d3e7c2b9e76c992855d0d3af970f9b58
dasfvdfas <1713476245@qq.com>
2024-10-18 15:37:39 +0800

    Entity of paper_management schema of user table.

---
+++ backend/src/main/java/com/library/backend/entity/PM_User.java
@@ -0,0 +1,21 @@
+package com.library.backend.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "user")
+public class PM_User {
+    @Id
+    private Long id;
+    private String name;
+    private String password;
+    private String email;
+    private String phone;
+    private String address;
+    private int role;
+}
