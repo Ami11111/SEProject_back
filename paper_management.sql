44cbbb749823482ed9c23b0a307e3aafd09b0e16
xuanxuan <407428197@qq.com>
2024-10-29 19:33:35 +0800

    up

--- paper_management.sql
+++ paper_management.sql
@@ -27,7 +27,7 @@ create table if not exists `user` (
 delete from `user`;
 insert into `user` values
 	(21808080, '8080', null, null, null, null, 1),
-    (21808081, '8081', 'chen', 'chen@fudan.edu.cn', '13998899889', '逸夫楼701-2', 1),
+    (21808081, '8081', 'chen', 'chen@fudan.edu.cn', '13998899889', '逸夫楼701-2', 0),
     (21606060, '6060', 'xuan', 'xuan@m.fudan.edu.cn', null, null, 0),
     (21606061, '6061', 'xiang', null, null, null, 0),
     (21606062, '6062', 'feng', null, null, null, 0),
@@ -36,50 +36,67 @@ insert into `user` values
 # table paper
 drop table if exists `paper`;
 create table if not exists `paper` (
-  `id` varchar(50) primary key,
-  `title` varchar(255) not null,
-  `Abstract` text comment '摘要',
-  `keywords` varchar(255) comment '关键词',
-  `publication_data` date comment '发表时间',
+  `doi` varchar(255) primary key comment 'DOI',
+  `title` varchar(255) not null comment '名称',
+  `author_names` varchar(255) not null comment '作者列表',
+  `first_author` varchar(255) not null comment '第一作者，所有',
+  `corresponding_author` varchar(255) comment '通讯作者',
   `page_count` int comment '论文页数',
-  `meeting` varchar(255) comment '会议名称',
-  `periodical` varchar(255) comment '期刊名称',
+  `conference_or_periodical` varchar(255) comment '会议期刊全称',
+  `acronym` varchar(50) comment '会议期刊简称',
   `publisher` varchar(255) comment '出版商',
-  `status` bit(2) default b'00' comment '未提交00，待审,01，通过10，驳回11',
-  `recommend` text comment '11选填，驳回意见'
+  `ccf` enum('A','B','C') comment 'CCF分区',
+  `fund` varchar(255) comment '基金',
+  `submit_time` date comment '提交时间',
+  `receipt_time` date comment '接收时间',
+  `publish_time` date comment '发表时间',
+  `type` enum('regular','short','demo','poster') comment '类型',
+  `file_data` mediumblob comment 'PDF文件',
+  `status` enum('not_submit','review','approve','reject') not null comment '未提交，审核中，通过，驳回',
+  `recommend` text comment '选填，驳回意见'
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 delete from `paper`;
 insert into `paper` values
-('F592;G353.1','基于山茶产业分析的旅游营销策略研究','balabala','山茶；旅游；营销策略','2023-01-10',9,null,null,null,b'00',null),
-('F274;F426.82','次优石榴干预营销的有效性：来自石榴村的证据','shiliushiliu','石榴；干预','2020-09-11',13,null,null,null,b'01',null),
-('G899;I207.41','从石猴到黑神话：悟空的形象变迁与跨媒介叙事','sunwukongsunwukong','黑神话；悟空','2024-08-23',99,null,'哈哈游月刊','哈哈游出版社',b'10',null),
-('F124.3','论生产力：特征与发力点','chanchanchan','生产力；特征；发力点','2020-12-27',10,'附中年会',null,null,b'11','内容不实');
+('10.1016/j.artint.2023.104057','Evolving interpretable decision trees for reinforcement learning',
+'chen,xiang;feng;liang;','chen,xiang;',null,26,'Artificial Intelligence','AI','MDPI AG','A',
+null,'2023-12-09','2023-12-10','2023-12-16','regular',null,'not_submit',null),
+('10.1145/3626238','Design and Validation of a Virtual Reality Mental Rotation Test',
+'feng;chen;liang;','feng',null,22,'ACM Transactions on Applied Perception','TAP','Association for Computing Machinery','B',
+null,null,null,'2024-01-01','regular',null,'review',null),
+('10.1016/j.cviu.2018.10.002','Visual tracking in video sequencesbasedonbiologically inspired mechanisms',
+'xiang;feng;liang;','xiang',null,22,'Computer Vision and Image Understanding','CVIU','Academic Press Inc.','B',
+null,'2018-10-05','2018-10-14','2018-10-26','regular',null,'approve',null),
+('10.1016/j.ijar.2024.109266','On the enumeration of non-dominated matroids with imprecise weights',
+'chen,liang;','chen,liang',null,23,'International Journal of Approximate Reasoning','IJAR','Elsevier','B',
+null,'2024-06-24','2024-07-31','2024-08-08','regular',null,'reject','内容不实');
 
 # table author_paper
 drop table if exists `author_paper`;
 create table if not exists `author_paper` (
  `a_id` int unsigned,
- `p_id` varchar(50),
- `seq` bit(2) not null comment '第一二三作者00,01,10',
+ `p_id` varchar(255),
+ `seq` enum('first','second','third') not null comment '第一二三作者',
  primary key (`a_id`,`p_id`),
  constraint `author_paper1` foreign key(`a_id`) references `user`(`id`), 
- constraint `author_paper2` foreign key(`p_id`) references `paper`(`id`), 
+ constraint `author_paper2` foreign key(`p_id`) references `paper`(`doi`), 
  index `a_id` (`a_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 delete from `author_paper`;
 insert into `author_paper` values
-(21808081,'F592;G353.1',b'00'),
-(21606061,'F592;G353.1',b'00'),
-(21606062,'F274;F426.82',b'00'),
-(21808081,'F274;F426.82',b'01'),
-(21606063,'F274;F426.82',b'10'),
-(21606061,'G899;I207.41',b'00'),
-(21606062,'G899;I207.41',b'01'),
-(21606063,'G899;I207.41',b'10'),
-(21808081,'F124.3',b'00'),
-(21606063,'F124.3',b'00');
+(21808081,'10.1016/j.artint.2023.104057','first'),
+(21606061,'10.1016/j.artint.2023.104057','first'),
+(21606062,'10.1016/j.artint.2023.104057','second'),
+(21606063,'10.1016/j.artint.2023.104057','third'),
+(21606062,'10.1145/3626238','first'),
+(21606061,'10.1145/3626238','second'),
+(21606063,'10.1145/3626238','third'),
+(21606061,'10.1016/j.cviu.2018.10.002','first'),
+(21606062,'10.1016/j.cviu.2018.10.002','second'),
+(21606063,'10.1016/j.cviu.2018.10.002','third'),
+(21808081,'10.1016/j.ijar.2024.109266','first'),
+(21606063,'10.1016/j.ijar.2024.109266','first');
 
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
