2fa795f353f139ada18b7dd52ad6d1c85ab7d35c
dasfvdfas <1713476245@qq.com>
2024-11-09 20:18:49 +0800

    合并peizijie的提交，完成论文上传功能

--- backend/src/main/java/com/library/backend/controller/PM_PaperController.java
+++ backend/src/main/java/com/library/backend/controller/PM_PaperController.java
@@ -1,32 +1,21 @@
 package com.library.backend.controller;
 
-import com.library.backend.entity.PM_Admin;
 import com.library.backend.entity.PM_Paper;
-import com.library.backend.entity.PM_User;
-import com.library.backend.model.Result;
 import com.library.backend.entity.PM_User;
 import com.library.backend.repository.PM_PaperRepository;
 import com.library.backend.repository.PM_UserRepository;
-import com.library.backend.utils.JwtUtil;
-import io.swagger.annotations.Api;
 import com.library.backend.utils.JwtUtil;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.transaction.Transactional;
-import java.util.Base64;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static com.library.backend.utils.Constant.*;
 
 @RestController
 @RequestMapping("/api")
@@ -35,9 +24,12 @@ public class PM_PaperController {
     private PM_PaperRepository paperRepository;
 
     @Autowired
+    private PM_UserRepository userRepository;
+
+    @Autowired
     private JwtUtil jwtUtil;
 
-    @PostMapping("/{doi}/file")
+    @PostMapping("/papers/{doi}/file")
     @ApiOperation(value = "上传论文文件")
     public ResponseEntity<Object> uploadPaperFile(@PathVariable("doi") String encodedDoi, @RequestParam("file") MultipartFile file, @RequestHeader("Authorization") String token) {
         Map<String, Object> response = new HashMap<>();
@@ -61,6 +53,12 @@ public class PM_PaperController {
             if (paperRepository.countByDoi(doi) == 0) {
                 response.put("message", "No paper found with DOI: " + doi);
                 return new ResponseEntity<>(response, HttpStatus.NOT_FOUND); // 404 Not Found
+            }
+            // 检查用户是否为doi对应论文的作者
+            String authorList = paperRepository.findByDoi(doi).get(0).getAuthorList();
+            if (!authorList.contains(user.getName())) {
+                response.put("message", "Access denied");
+                return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED); // 401 状态码
             }
             // 上传文件
             byte[] fileData = file.getBytes();
@@ -110,23 +108,23 @@ public class PM_PaperController {
         }
     }
 
-    @GetMapping("/papers/{author}")
-    @ApiOperation(value = "根据作者查询论文")
-    public ResponseEntity<Object> findByAuthor(@RequestParam String author) {
-        Map<String, Object> response = new HashMap<>();
-        try {
-            List<PM_Paper> papers = paperRepository.findByAuthorListContaining(author, author, author);
-            if (!papers.isEmpty()) {
-                response.put("message", "Success");
-                response.put("papers", papers);
-                return new ResponseEntity<>(response, HttpStatus.OK);
-            } else {
-                response.put("message", "未找到该作者的论文");
-                return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            response.put("message", e.toString());
-            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-        }
-    }
+    //@GetMapping("/papers/{author}")
+    //@ApiOperation(value = "根据作者查询论文")
+    //public ResponseEntity<Object> findByAuthor(@RequestParam String author) {
+    //    Map<String, Object> response = new HashMap<>();
+    //    try {
+    //        List<PM_Paper> papers = paperRepository.findByAuthorListContaining(author, author, author);
+    //        if (!papers.isEmpty()) {
+    //            response.put("message", "Success");
+    //            response.put("papers", papers);
+    //            return new ResponseEntity<>(response, HttpStatus.OK);
+    //        } else {
+    //            response.put("message", "未找到该作者的论文");
+    //            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    //        }
+    //    } catch (Exception e) {
+    //        response.put("message", e.toString());
+    //        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    //    }
+    //}
 }
