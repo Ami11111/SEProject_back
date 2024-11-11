74128f55a150326b6c47cc2741c30cb5d208ee7d
xuanxuan <407428197@qq.com>
2024-11-11 18:33:59 +0800

    up

---
+++ backend/src/main/java/com/library/backend/config/SecurityFirewallConfig.java
@@ -0,0 +1,22 @@
+package com.library.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
+
+@Order(Ordered.LOWEST_PRECEDENCE)
+@Configuration
+public class SecurityFirewallConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
+        DefaultHttpFirewall firewall = new DefaultHttpFirewall();
+        firewall.setAllowUrlEncodedSlash(true);
+        return firewall;
+    }
+}
+
+
