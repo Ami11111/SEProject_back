a6f9475e46bbdc068a3ee4178e11a63dfea53245
xuanxuan <407428197@qq.com>
2024-11-12 19:01:26 +0800

    up

--- backend/src/main/java/com/library/backend/controller/PM_PaperClaimApplicationController.java
+++ backend/src/main/java/com/library/backend/controller/PM_PaperClaimApplicationController.java
@@ -7,6 +7,7 @@ import com.library.backend.service.PaperService;
 import com.library.backend.utils.JwtUtil;
 import com.library.backend.utils.PaperUtil;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
